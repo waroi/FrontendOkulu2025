78f8f7521628f7a15060ec5ba1914db1f9b873f9
waroi <varolmaksutoglu@yandex.com>
2025-10-12 12:00:09 +0300

    mulakat

---
+++ Dersler/Ders006-Js-Prototype-Class/05-Mulakat/app.js
@@ -0,0 +1,30 @@
+// console.log("one");
+// setTimeout(function () {
+//   console.log("two");
+// }, 0);
+// console.log("three");
+
+// var foo = { n: 1 };
+// var bar = foo;
+// foo.x = foo = { n: 2 };
+// console.log(foo.x);
+
+// var x = 500;
+// let y, z, p, q;
+// q = 200;
+// if (true) {
+//   q = y = z = p = x;
+//   document.getElementById("display").innerHTML =
+//     "x=" + x + "y :" + y + "z :" + z + "p :" + p + "q :" + q;
+// }
+
+// let c = { greeting: "Hey!" };
+// let d;
+
+// d = c;
+// c.greeting = "Hello";
+// console.log(d.greeting);
+
+// var ile let arasındaki en az iki fark nedir?
+// 1- var function scope'a sahiptir, let ise block scope'a sahiptir.
+// 2- var ile tanımlanan değişkenler hoisting'e tabidir, let ile tanımlananlar ise hoisting'e tabi değildir ve tanımlanmadan önce erişilmeye çalışılırsa ReferenceError hatası verir.
